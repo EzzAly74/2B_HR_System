738f5a674e994a4f719aa56cb0cf015b1e560fce
OmarKhaled192 <okoros192000@gmail.com>
2025-01-08 04:26:52 +0200

    add-all

--- src/app/demo/components/info/components/custom/applying-internal-job/applying-internal-job.service.ts
+++ src/app/demo/components/info/components/custom/applying-internal-job/applying-internal-job.service.ts
@@ -4,15 +4,22 @@ import { Observable } from 'rxjs';
 import { LockupsService } from 'src/app/demo/service/lockups.service';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root',
 })
 export class ApplyingInternalJobService extends LockupsService {
-  constructor(http: HttpClient) {
-    super(http);
-  }
-  getDropdownField(field: any): Observable<any> {
-    return this.http.get(
-      `${this.baseurl}/${field}/getDropDown/?culture=${this.culture}`
-    );
-  }
+    constructor(http: HttpClient) {
+        super(http);
+    }
+    getDropdownField(field: any): Observable<any> {
+        return this.http.get(
+            `${this.baseurl}/${field}/getDropDown/?culture=${this.culture}`
+        );
+    }
+
+    changeStatus(body: any): Observable<any> {
+        return this.http.post(
+            `${this.baseurl}/${this.endPoint}/HRAccept`,
+            body
+        );
+    }
 }
