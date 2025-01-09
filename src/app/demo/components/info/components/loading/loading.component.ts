d4de2b957e65c6d2c573ec12fed29ddc0b04d170
omarkhaled192 <okoros192000@gmail.com>
2025-01-09 18:56:40 +0200

    update-loading-translate

--- src/app/demo/components/info/components/loading/loading.component.ts
+++ src/app/demo/components/info/components/loading/loading.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+import { TreeTableService } from 'primeng/treetable';
 
 @Component({
   selector: 'pre-loading',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './loading.component.html',
-  styleUrl: './loading.component.scss'
+  styleUrl: './loading.component.scss',
+  providers: [TreeTableService]
 })
 export class LoadingComponent {
 
